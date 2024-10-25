22e8be561ca4c580983041591b36566e8539b2b6
Alexandru Puscasu <palexandru2000@gmail.com>
2024-10-25 09:36:07 +0300

    speed up tb

--- test/tb_memory_operations.sv
+++ test/tb_memory_operations.sv
@@ -7,39 +7,16 @@ localparam ADDR_WIDTH   = 32'd32;
 localparam DATA_WIDTH   = 32'd128;
 localparam DATA_BYTES   = DATA_WIDTH / 8;
 
-localparam int unsigned TbNumMasters        = 32'd2;
-localparam int unsigned TbNumSlaves         = 32'd1;
-localparam int unsigned TbNumWrites         = 32'd200;
-localparam int unsigned TbNumReads          = 32'd200;
-localparam int unsigned TbAxiIdWidthMasters = 32'd5;
-localparam int unsigned TbAxiIdUsed         = 32'd3;
+localparam int unsigned TbAxiIdWidth        = 32'd5;
 localparam int unsigned TbAxiDataWidth      = 32'd128;
-localparam int unsigned TbPipeline          = 32'd1;
-localparam bit          TbEnAtop            = 1'b1;
-localparam bit TbEnExcl                     = 1'b0;
-localparam bit TbUniqueIds                  = 1'b0;
-localparam int unsigned TbAxiIdWidthSlaves =  TbAxiIdWidthMasters + $clog2(TbNumMasters);
-localparam int unsigned TbAxiAddrWidth     =  32'd32;
-localparam int unsigned TbAxiStrbWidth     =  TbAxiDataWidth / 8;
-localparam int unsigned TbAxiUserWidth     =  5;
-localparam NUMBER_OF_REGISTERS = 32;
+localparam int unsigned TbAxiAddrWidth      = 32'd32;
+localparam int unsigned TbAxiStrbWidth      = TbAxiDataWidth / 8;
+localparam int unsigned TbAxiUserWidth      = 5;
+localparam              NUMBER_OF_REGISTERS = 32;
 
-localparam axi_pkg::xbar_cfg_t xbar_cfg = '{
-    NoSlvPorts:         TbNumMasters,
-    NoMstPorts:         TbNumSlaves,
-    MaxMstTrans:        10,
-    MaxSlvTrans:        6,
-    FallThrough:        1'b0,
-    LatencyMode:        axi_pkg::CUT_ALL_AX,
-    PipelineStages:     TbPipeline,
-    AxiIdWidthSlvPorts: TbAxiIdWidthMasters,
-    AxiIdUsedSlvPorts:  TbAxiIdUsed,
-    UniqueIds:          TbUniqueIds,
-    AxiAddrWidth:       TbAxiAddrWidth,
-    AxiDataWidth:       TbAxiDataWidth,
-    NoAddrRules:        TbNumSlaves
-};
+logic clk, rst_n;
 
+// dut controll signals
 logic                                       valid    ;
 logic                                       ready    ;
 logic                                       arth_data;
@@ -56,131 +33,50 @@ logic               [ADDR_WIDTH - 1 : 0]    width    ;
 logic               [ADDR_WIDTH - 1 : 0]    height   ;
 ma_pkg::dtype                               dtype    ;
 logic                                       error    ;
-
-typedef axi_pkg::xbar_rule_32_t         rule_t; // Has to be the same width as axi addr
-
-// Each slave has its own address range:
-localparam rule_t [xbar_cfg.NoAddrRules-1:0] AddrMap = addr_map_gen();
-
-function rule_t [xbar_cfg.NoAddrRules-1:0] addr_map_gen ();
-for (int unsigned i = 0; i < xbar_cfg.NoAddrRules; i++) begin
-    addr_map_gen[i] = rule_t'{
-    idx:        unsigned'(i),
-    start_addr: 32'h0000_0000,
-    end_addr:   32'hffff_ffff,
-    default:    '0
-    };
-end
-endfunction
-
-logic clk, rst_n;
-
-logic write_valid, write_ready, write_done;
-logic write_data_ready, write_data_valid;
-logic [ADDR_WIDTH - 1 : 0]  write_addr, write_len;
-logic [DATA_WIDTH - 1 : 0]  write_data;
-
-logic read_valid, read_ready, read_done;
-logic read_data_ready, read_data_valid;
-logic [ADDR_WIDTH - 1 : 0]  read_addr, read_len;
-logic [DATA_WIDTH - 1 : 0]  read_data;
-
-logic [2 : 0]     funct3;
+logic [2 : 0]                               funct3   ;
 
 localparam MEM_SIZE = 1024 * 1024; // 1 MB
-
-logic [7 : 0] mem [MEM_SIZE - 1 : 0];
 
 function void init_mem();
   int i;
   for (i = 0; i < MEM_SIZE; i = i + 1)
-    mem[i] = i[7:0];
+    i_sim_mem.i_sim_mem.mem[i] = i[7:0];
 endfunction
 
-task automatic dma_axi_write;
-input int bytes; 
-input int addr;
-begin
-    int i;
-    write_addr <= addr;
-    write_len <= bytes;
-    write_valid <= 1'b1;
-    @(posedge clk)
-    while (write_ready != 1'b1) @(posedge clk);
-    write_valid <= 1'b0;
-
-    @(posedge clk);
-    for (int i = 0; i < bytes / DATA_BYTES + |bytes[$clog2(DATA_BYTES) - 1 : 0];) begin
-      if ( write_data_ready == 1'b1 ) begin
-        write_data <= { >> {mem[addr + i * DATA_BYTES +: DATA_BYTES]}};
-        write_data_valid <= 1'b1;
-        i = i + 1;
-      end
-      @(posedge clk);
-    end
-    write_data_valid <= 1'b0;
-    write_data = 'dx;
-
-    wait(write_done);
-end
-endtask
-
-  AXI_BUS #(
-    .AXI_ADDR_WIDTH ( TbAxiAddrWidth      ),
-    .AXI_DATA_WIDTH ( TbAxiDataWidth      ),
-    .AXI_ID_WIDTH   ( TbAxiIdWidthMasters ),
-    .AXI_USER_WIDTH ( TbAxiUserWidth      )
-  ) master [TbNumMasters-1:0] ();
-
-  AXI_BUS #(
-    .AXI_ADDR_WIDTH ( TbAxiAddrWidth     ),
-    .AXI_DATA_WIDTH ( TbAxiDataWidth     ),
-    .AXI_ID_WIDTH   ( TbAxiIdWidthSlaves ),
-    .AXI_USER_WIDTH ( TbAxiUserWidth     )
-  ) slave [TbNumSlaves-1:0] ();
-
-axi_xbar_intf #(
-    .AXI_USER_WIDTH ( TbAxiUserWidth  ),
-    .Cfg            ( xbar_cfg        ),
-    .rule_t         ( rule_t          )
-) i_xbar (
-    .clk_i                  ( clk     ),
-    .rst_ni                 ( rst_n   ),
-    .test_i                 ( 1'b0    ),
-    .slv_ports              ( master  ),
-    .mst_ports              ( slave   ),
-    .addr_map_i             ( AddrMap ),
-    .en_default_mst_port_i  ( '0      ),
-    .default_mst_port_i     ( '0      )
-);
+AXI_BUS #(
+  .AXI_ADDR_WIDTH ( TbAxiAddrWidth      ),
+  .AXI_DATA_WIDTH ( TbAxiDataWidth      ),
+  .AXI_ID_WIDTH   ( TbAxiIdWidth        ),
+  .AXI_USER_WIDTH ( TbAxiUserWidth      )
+) axi ();
 
 axi_sim_mem_intf #(
-    .AXI_ADDR_WIDTH      ( ADDR_WIDTH ),
-    .AXI_DATA_WIDTH      ( DATA_WIDTH ),
-    .AXI_ID_WIDTH        ( TbAxiIdWidthSlaves ),
-    .AXI_USER_WIDTH      ( TbAxiUserWidth     ),
-    .WARN_UNINITIALIZED  ( 1'b0       ),
-    .APPL_DELAY          ( 2ns        ),
-    .ACQ_DELAY           ( 8ns        )
-  ) i_sim_mem (
-    .clk_i             (clk     ),
-    .rst_ni            (rst_n   ),
-    .axi_slv           (slave[0]),
-    .mon_w_valid_o     (        ),
-    .mon_w_addr_o      (        ),
-    .mon_w_data_o      (        ),
-    .mon_w_id_o        (        ),
-    .mon_w_user_o      (        ),
-    .mon_w_beat_count_o(        ),
-    .mon_w_last_o      (        ),
-    .mon_r_valid_o     (        ),
-    .mon_r_addr_o      (        ),
-    .mon_r_data_o      (        ),
-    .mon_r_id_o        (        ),
-    .mon_r_user_o      (        ),
-    .mon_r_beat_count_o(        ),
-    .mon_r_last_o      (        )
-  );
+  .AXI_ADDR_WIDTH      ( ADDR_WIDTH ),
+  .AXI_DATA_WIDTH      ( DATA_WIDTH ),
+  .AXI_ID_WIDTH        ( TbAxiIdWidth ),
+  .AXI_USER_WIDTH      ( TbAxiUserWidth     ),
+  .WARN_UNINITIALIZED  ( 1'b0       ),
+  .APPL_DELAY          ( 2ns        ),
+  .ACQ_DELAY           ( 8ns        )
+) i_sim_mem (
+  .clk_i             (clk     ),
+  .rst_ni            (rst_n   ),
+  .axi_slv           (axi     ),
+  .mon_w_valid_o     (        ),
+  .mon_w_addr_o      (        ),
+  .mon_w_data_o      (        ),
+  .mon_w_id_o        (        ),
+  .mon_w_user_o      (        ),
+  .mon_w_beat_count_o(        ),
+  .mon_w_last_o      (        ),
+  .mon_r_valid_o     (        ),
+  .mon_r_addr_o      (        ),
+  .mon_r_data_o      (        ),
+  .mon_r_id_o        (        ),
+  .mon_r_user_o      (        ),
+  .mon_r_beat_count_o(        ),
+  .mon_r_last_o      (        )
+);
 
 opcode_decoder i_op_decoder (
     .opcode     ( 7'h2b ) ,
@@ -199,7 +95,7 @@ ma_data_path #(
 ) i_dut (
     .aclk       ( clk ),
     .arst_n     ( rst_n ),
-    .axi        ( master[0] ),
+    .axi        ( axi ),
     .clk        ( clk ),
     .rst_n      ( rst_n ),
     .valid      ( valid     ),
@@ -229,33 +125,6 @@ generate
     assign buffers_clone[i] = i_dut.memory_bank[i].i_mem_bank.mem;
   end
 endgenerate
-
-dma #(
-    .ADDR_WIDTH ( ADDR_WIDTH )  ,
-    .DATA_WIDTH ( DATA_WIDTH )  
-) i_dma (
-    .clk                 ( clk ),
-    .rst_n               ( rst_n ),
-    .write_valid_i       ( write_valid ),
-    .write_ready_o       ( write_ready ),
-    .write_addr_i        ( write_addr ),
-    .write_len_i         ( write_len ),
-    .write_done_o        ( write_done ),
-    .read_valid_i        ( read_valid ),
-    .read_ready_o        ( read_ready ),
-    .read_addr_i         ( read_addr ),
-    .read_len_i          ( read_len ),
-    .read_done_o         ( read_done ),
-    .write_data_i        ( write_data ),
-    .write_data_valid_i  ( write_data_valid ),
-    .write_data_ready_o  ( write_data_ready ),
-    .read_data_o         ( read_data ),
-    .read_data_valid_o   ( read_data_valid ),
-    .read_data_ready_i   ( read_data_ready ),
-    .aclk                ( clk )       ,
-    .arst_n              ( rst_n )       ,
-    .axi                 ( master[1] )
-);
 
 clk_rstn i_clk_gen (.clk, .rst_n);
 
@@ -298,8 +167,9 @@ task load_register;
     input logic [4 : 0] r;
     input int addr;
 begin
-    int bytes, i;
+    int bytes, i, j;
     bit pass;
+    logic [7 : 0] line [DATA_BYTES - 1 : 0];
 
     $display("Load register");
 
@@ -330,8 +200,12 @@ begin
       bytes = bytes * 4;
 
     pass = 1'b1;
-    for ( i = 0; i < bytes; i = i + DATA_BYTES )
-      pass = pass & (buffers_clone[r][i / DATA_BYTES] == { >> {mem[addr + i +: DATA_BYTES]}});
+    for ( i = 0; i < bytes; i = i + DATA_BYTES ) begin
+      for ( j = 0; j < DATA_BYTES; j = j + 1 ) begin
+        line[j] = i_sim_mem.i_sim_mem.mem[addr + i + j];
+      end
+      pass = pass & (buffers_clone[r][i / DATA_BYTES] == { >> {line}});
+    end
     assert(pass);
 end
 endtask
@@ -475,18 +349,6 @@ endtask
 int register;
 
 initial begin
-    // dma
-    write_data = 'dx;
-    write_valid <= 1'b0;
-    write_data_valid <= 1'b0;
-    write_addr <= 'd0;
-    write_len <= 'd0;
-
-    read_valid <= 1'b0;
-    read_data_ready <= 1'b0;
-    read_addr <= 'd0;
-    read_len <= 'd0;
-
     // ccu
     valid       <= 'd0;
     dut_addr    <= 'd0;
@@ -501,12 +363,11 @@ initial begin
 
     @(posedge rst_n);
 
-    init_mem();
+    @(posedge axi.aw_ready);
+    @(posedge clk);
+    @(posedge clk);
 
-    @(posedge slave[0].aw_ready);
-    @(posedge clk);
-    @(posedge clk);
-    dma_axi_write(MEM_SIZE / 2, 0); // init memory
+    init_mem();
 
     for ( register = 0; register < 32; register = register + 1 )
       define_register(16, 16, 'd0, register);
