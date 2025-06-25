abe1fb369bed0542d90d5a8f35bd6714683ad32f
Alexandru Puscasu <palexandru2000@gmail.com>
2025-06-25 06:54:28 +0000

    cnv addrr gen for 32b

---
+++ src/rtl/convolution_unit/matrix_addr_gen.sv
@@ -0,0 +1,179 @@
+module matrix_addr_gen #(
+    parameter   PRF_LOG_P   =   1   ,
+    parameter   PRF_LOG_Q   =   2   ,
+    parameter   PRF_LOG_N   =   10  ,
+    parameter   PRF_LOG_M   =   10  ,
+    parameter   SRAM_WIDTH  =   32  
+) (
+    input   logic                                               clk     ,
+    input   logic                                               rst_n   ,
+    input   logic                                               en      ,
+    input   logic                                               start   ,
+    input   logic                                               incr    ,
+    input   ma_pkg::register_file_line_t                        r       ,
+    input   ma_pkg::register_file_line_t                        r_k     ,
+    output  logic                        [PRF_LOG_N - 1 : 0]    i_out   ,
+    output  logic                        [PRF_LOG_M - 1 : 0]    j_out   ,
+    output  logic                                               done    
+);
+
+// Local Parameters Definition  ------------------------------------------------------------------------------
+localparam PRF_N_LANES  = 2 ** (PRF_LOG_P + PRF_LOG_Q);
+
+
+// Wires Definition ------------------------------------------------------------------------------------------
+logic   [PRF_LOG_P + PRF_LOG_Q - 1 : 0] iteration;
+logic iteration_done, kernel_next;
+
+logic   [PRF_LOG_N : 0]    i_out_next, i_limit, i_start, i_start_next;
+logic   [PRF_LOG_M : 0]    j_out_next, j_limit, j_start, j_start_next;
+logic   [PRF_LOG_N : 0]    i_kernel, i_kernel_next, i_kernel_limit;
+logic   [PRF_LOG_M : 0]    j_kernel, j_kernel_next, j_kernel_limit;
+logic i_done, j_done, i_kernel_done, j_kernel_done, kernel_done;
+
+
+// Combinatorial Logic ---------------------------------------------------------------------------------------
+assign i_kernel_next = i_kernel + (2 ** PRF_LOG_P);
+assign j_kernel_next = j_kernel + (2 ** PRF_LOG_Q);
+assign i_done = i_start - r.prf_x[PRF_LOG_N - 1 : 0] > i_limit;
+assign j_done = j_out - j_kernel - r.prf_y[PRF_LOG_M - 1 : 0] >= j_limit;
+assign i_kernel_done = i_kernel_next >= i_kernel_limit;
+assign j_kernel_done = j_kernel_next >= j_kernel_limit;
+assign done = en & incr & i_done & j_done & iteration_done & (~|i_kernel) & (~|j_kernel);
+assign iteration_done = &iteration;
+assign kernel_next = &iteration[PRF_LOG_P + PRF_LOG_Q - 1 : 1] & ~iteration[0] & incr;
+assign kernel_done = i_kernel_done & j_kernel_done;
+
+always_comb begin
+    i_out_next = i_out;
+    if ( iteration_done ) begin
+        i_out_next = i_start + i_kernel;
+    end
+end
+
+always_comb begin
+    j_out_next = j_out;
+    if ( iteration_done ) begin
+            j_out_next = j_start + j_kernel;
+    end else begin
+        if ( ~j_done )
+            j_out_next = j_out + 1'b1;    
+    end
+end
+
+always_comb begin
+    i_start_next = i_start;
+    if ( kernel_done & j_done )
+        i_start_next = i_start + 1'b1;
+end
+
+always_comb begin
+    j_start_next = j_start;
+    if ( kernel_done ) begin
+        if ( j_done )
+            j_start_next = r.prf_y[PRF_LOG_M - 1 : 0];
+        else
+            j_start_next = j_out + 2'd2 - j_kernel;
+    end
+end
+
+
+// Sequential Logic ------------------------------------------------------------------------------------------
+always_ff @( posedge clk, negedge rst_n )
+    if ( ~rst_n ) begin
+        i_limit <= 'd0;
+        j_limit <= 'd0;
+    end else if ( en & start ) begin
+        i_limit <= r.height[PRF_LOG_N : 0] - r_k.height[PRF_LOG_N : 0];
+        if ( r.dtype == ma_pkg::UINT32 || r.dtype == ma_pkg::INT32 ) begin
+            j_limit <= r.width[PRF_LOG_M : 0] - r_k.width[PRF_LOG_M : 0];
+        end else if ( r.dtype == ma_pkg::UINT16 || r.dtype == ma_pkg::INT16 ) begin
+            j_limit <= r.width[PRF_LOG_M + 1 : 1] - r_k.width[PRF_LOG_M + 1 : 1];
+        end else begin
+            j_limit <= r.width[PRF_LOG_M + 2 : 2] - r_k.width[PRF_LOG_M + 2 : 2];
+        end
+    end
+
+always_ff @( posedge clk, negedge rst_n )
+    if ( ~rst_n ) begin
+        i_kernel_limit <= 'd0;
+        j_kernel_limit <= 'd0;
+    end else if ( en & start ) begin
+        i_kernel_limit <= r_k.height[PRF_LOG_N : 0];
+        if ( r.dtype == ma_pkg::UINT32 || r.dtype == ma_pkg::INT32 ) begin
+            j_kernel_limit <= r_k.width[PRF_LOG_M : 0];
+        end else if ( r.dtype == ma_pkg::UINT16 || r.dtype == ma_pkg::INT16 ) begin
+            j_kernel_limit <= r_k.width[PRF_LOG_M + 1 : 1];
+        end else begin
+            j_kernel_limit <= r_k.width[PRF_LOG_M + 2 : 2];
+        end
+    end
+
+always_ff @( posedge clk, negedge rst_n )
+    if ( ~rst_n )                   i_kernel <= 'd0;                        else
+    if ( done )                     i_kernel <= 'd0;                        else
+    if ( en ) begin
+        if ( start )                i_kernel <= 'd0;                        else
+        if ( kernel_next ) begin 
+            if ( kernel_done )      i_kernel <= 'd0;                        else
+            if ( j_kernel_done )    i_kernel <= i_kernel_next;
+        end
+    end
+
+always_ff @( posedge clk, negedge rst_n )
+    if ( ~rst_n )                   j_kernel <= 'd0;                        else
+    if ( done )                     j_kernel <= 'd0;                        else
+    if ( en ) begin
+        if ( start )                j_kernel <= 'd0;                        else
+        if ( kernel_next ) begin
+            if ( j_kernel_done )    j_kernel <= 'd0;                        else
+                                    j_kernel <= j_kernel_next;
+        end
+    end
+
+always_ff @( posedge clk, negedge rst_n )
+    if ( ~rst_n )                   iteration <= 'd0;                       else
+    if ( done )                     iteration <= 'd0;                       else
+    if ( en ) begin
+        if ( start )                iteration <= 'd0;                       else
+        if ( incr )                 iteration <= iteration + 1'b1;
+    end
+
+always_ff @( posedge clk, negedge rst_n )
+    if ( ~rst_n )                   i_out <= 'd0;                           else
+    if ( done )                     i_out <= 'd0;                           else
+    if ( en ) begin
+        if ( start )                i_out <= r.prf_x[PRF_LOG_N - 1 : 0];    else
+        if ( incr  )                i_out <= i_out_next;       
+    end
+
+always_ff @( posedge clk, negedge rst_n )
+    if ( ~rst_n )                   j_out <= 'd0;                           else
+    if ( done )                     j_out <= 'd0;                           else
+    if ( en ) begin
+        if ( start )                j_out <= r.prf_y[PRF_LOG_M - 1 : 0];    else
+        if ( incr )                 j_out <= j_out_next;
+    end
+
+always_ff @( posedge clk, negedge rst_n )
+    if ( ~rst_n )                   i_start <= 'd0;                         else
+    if ( done )                     i_start <= 'd0;                         else
+    if ( en ) begin
+        if ( start )                i_start <= r.prf_x[PRF_LOG_N - 1 : 0];  else
+        if ( kernel_next )          i_start <= i_start_next;
+    end
+
+always_ff @( posedge clk, negedge rst_n )
+    if ( ~rst_n )                   j_start <= 'd0;                         else
+    if ( done )                     j_start <= 'd0;                         else
+    if ( en ) begin
+        if ( start )                j_start <= r.prf_y[PRF_LOG_M - 1 : 0];  else
+        if ( kernel_next )          j_start <= j_start_next;
+    end
+   
+
+// Modules Instances -----------------------------------------------------------------------------------------
+
+
+
+endmodule
---
+++ src/tests/tb_cnv.sv
@@ -0,0 +1,96 @@
+`timescale 1ns/1ns
+
+module tb_cnv();
+
+localparam PRF_LOG_N = 10;
+localparam PRF_LOG_M = 10;
+
+logic                                               clk  ;
+logic                                               rst_n;
+logic                                               en   ;
+logic                                               start;
+logic                                               incr ;
+ma_pkg::register_file_line_t                        r    ;
+ma_pkg::register_file_line_t                        r_k  ;
+logic                        [PRF_LOG_N - 1 : 0]    i_out;
+logic                        [PRF_LOG_M - 1 : 0]    j_out;
+logic                                               done ;
+
+matrix_addr_gen #(
+    .PRF_LOG_P  ( 1         ),
+    .PRF_LOG_Q  ( 1         ),
+    .PRF_LOG_N  ( PRF_LOG_N ),
+    .PRF_LOG_M  ( PRF_LOG_M ),
+    .SRAM_WIDTH ( 32        )
+) i_dut (
+    .clk     ,
+    .rst_n   ,
+    .en      ,
+    .start   ,
+    .incr    ,
+    .r       ,
+    .r_k     ,
+    .i_out   ,
+    .j_out   ,
+    .done    
+);
+
+initial begin
+    clk = 1'b1;
+    forever #5 clk = ~clk;
+end
+
+initial begin
+    rst_n = 1'b0;
+    repeat(3) @(posedge clk);
+    @(negedge clk);
+    rst_n = 1'b1;
+end
+
+initial begin
+    incr = 1'b1;
+    en = 1'b0;
+    start = 1'b0;
+
+    r.width = 8;
+    r.height = 4;
+    r.dtype = ma_pkg::INT32;
+    r.prf_x = 0;
+    r.prf_y = 0;
+
+    r_k.width = 2;
+    r_k.height = 2;
+    r.dtype = ma_pkg::INT32;
+
+    repeat(5) @(posedge clk);
+    en = 1'b1;
+    start = 1'b1;
+    @(posedge clk);
+    start = 1'b0;
+
+    @(posedge clk)
+    wait(done);
+    @(posedge clk);
+    en = 1'b0;
+
+    r_k.width = 3;
+    r_k.height = 3;
+    r.dtype = ma_pkg::INT32;
+
+    repeat(5) @(posedge clk);
+    en = 1'b1;
+    start = 1'b1;
+    @(posedge clk);
+    start = 1'b0;
+
+    @(posedge clk);
+    wait(done);
+    @(posedge clk);
+    en = 1'b0;
+
+    @(posedge clk);
+    $stop();
+
+end
+
+endmodule
